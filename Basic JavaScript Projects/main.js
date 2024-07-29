8c694755c5183d6fa1770e2687ff18f700a00a2c
Craig Hennessy <121065913+SuperSaiyan27@users.noreply.github.com>
2024-07-29 20:24:03 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/main.js
@@ -0,0 +1,18 @@
+//main.js
+
+//window.alert method
+window.alert("Welcome, Saiyan!");
+
+//document.write mathod
+document.write("This is the end of your journey Kakarot!");
+
+//creating two variables
+var Sent1 = "The Z Fighters once again save planet Earth!";
+
+var Sent2 = "A new Saiyan threat has arrived! Nappa and the might Vegeta reach Earth!";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write("A new threat!");
