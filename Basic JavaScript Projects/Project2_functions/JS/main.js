1633d1428901750dfd71aa52a75427e812f1a5ce
Craig Hennessy <121065913+SuperSaiyan27@users.noreply.github.com>
2024-08-19 13:10:54 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+//main.js
+
+//a function that ueses the += operator
+function myFunction() {
+    var sentence = "Hi!, My names Goku"; //creating the variable
+    sentence += "I heard you are very strong!"; //using += operator to add the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementId()
+}
